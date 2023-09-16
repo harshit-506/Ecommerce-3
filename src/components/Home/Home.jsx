a1b41ef24b241131e3966e70ee46c403e84bbb2d
harshit-506 <122781586+harshit-506@users.noreply.github.com>
2023-09-17 00:57:45 +0530

    day8

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 import "./Home.scss"
 import Banner from "./Banner/Banner";
 import Category from './Category/Category';
 import Products from '../Products/Products';
+import { fetchDataFromApi } from '../../utils/api';
 function Home() {
+
+  useEffect(()=>{
+   getCategories();
+  })
+    
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories")
+      .then((res) => {
+        // Handle the response data here
+        console.log(res);
+      })
+      .catch((error) => {
+        // Handle any errors here
+        console.error(error);
+      });
+  };
+  
+
+
   return (
     <div>
     <Banner/>
